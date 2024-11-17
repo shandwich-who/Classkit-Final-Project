88be40ec1c0df1c5eb6ed96ad04aff779a294a85
Yume-Irido <galangsean1@gmail.com>
2024-11-17 09:20:10 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:finals/login-folder/login_scaffold.dart';
+import 'package:finals/signup-folder/signup_scaffold.dart';
 import 'package:finals/splash-screen-folder/splash_screen_scaffold.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     // const LoginScaffold()
-    const SplashScreenScaffold()
+    // const SplashScreenScaffold()
+    const SignupScaffold()
   );
 }
 
---
+++ lib/signup-folder/signup_section.dart
@@ -0,0 +1,176 @@
+import 'package:finals/login-folder/login_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class SignupSection extends StatefulWidget {
+  const SignupSection({super.key});
+
+  @override
+  State<SignupSection> createState() => _SignupSectionState();
+}
+
+class _SignupSectionState extends State<SignupSection> {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    final TextEditingController confirmPasswordController =
+        TextEditingController();
+
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //Todo image
+                      Image.asset(
+                        "images/signup.png",
+                        height: 220,
+                        width: 220,
+                      ),
+                      //   const Text(
+                      //     "Sign Up",
+                      //     style: TextStyle(
+                      //         fontSize: 20,
+                      //         fontWeight: FontWeight.bold,
+                      //         color: Color(0xff013237),
+                      //         fontFamily: "Poppins"),
+                      //   ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: const TextStyle(color: Color(0xff1a1a1a)),
+                      hintText: "Enter your Email",
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff1a1a1a), width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Color(0xff1a1a1a), width: 1),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: "Enter your Password",
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      labelStyle: const TextStyle(color: Color(0xff1a1a1a)),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff1a1a1a), width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Color(0xff1a1a1a), width: 1),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  TextField(
+                    controller: confirmPasswordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      hintText: "Re-enter your Password",
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      labelStyle: const TextStyle(color: Color(0xff1a1a1a)),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff1a1a1a), width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Color(0xff1a1a1a), width: 1),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      //   String email = emailController.text;
+                      //   String password = passwordController.text;
+                      //   print('Email: $email, Password: $password');
+                      // TODO: Implement login logic here
+                    },
+                    style: ElevatedButton.styleFrom(
+                       backgroundColor: const Color(0xff4ca771),
+                      foregroundColor: const Color(0xffffffff),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      minimumSize: const Size(double.infinity, 50),
+                      elevation: 4,
+                    ),
+                    child: const Text('Create Account'),
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Already have an account ?",
+                        style: TextStyle(
+                          color: Color(0xff1a1a1a),
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      GestureDetector(
+                        onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder:  (context) => const LoginScaffold())) ;
+                        },
+                        child: const Text(
+                          "Sign in here!",
+                          style: TextStyle(
+                            color: Color(0xff4ca771),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
