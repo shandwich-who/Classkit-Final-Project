e98a795bd7ae831255e4452e775abde1039fadd6
Yume-Irido <galangsean1@gmail.com>
2024-11-27 17:23:28 +0800

    successfully fix the bug in sign up

--- lib/show-message-folder/show_message.dart
+++ lib/show-message-folder/show_message.dart
@@ -1,9 +1,8 @@
 import 'package:animated_snack_bar/animated_snack_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 void showMessage(
-    {required BuildContext context,
+    {required BuildContext? context,
     String? message,
     AnimatedSnackBarType? typeColor,
     Duration? duration}) {
@@ -12,88 +11,5 @@ void showMessage(
     type: typeColor!,
     borderRadius: BorderRadius.circular(16),
     animationDuration: duration!,
-  ).show(context);
-}
-
-class ShowSnackBarMessage {
-  static hideSnackBar() =>
-      ScaffoldMessenger.of(Get.context!).hideCurrentSnackBar();
-
-  static customToast({required message}) {
-    ScaffoldMessenger.of(Get.context!).showSnackBar(
-      SnackBar(
-        elevation: 0,
-        duration: const Duration(seconds: 3),
-        backgroundColor: Colors.transparent,
-        content: Container(
-          padding: const EdgeInsets.all(12.0),
-          margin: const EdgeInsets.symmetric(horizontal: 30),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Color(0xffaaaaaa),
-          ),
-        ),
-      ),
-    );
-  }
-
-  static successSnackBar({required title, message = "", duration = 3}) {
-    Get.snackbar(
-      title,
-      message,
-      isDismissible: true,
-      shouldIconPulse: true,
-      colorText: Colors.white,
-      backgroundColor: Color(0xff22bb33),
-      duration: duration,
-      snackPosition: SnackPosition.BOTTOM,
-      margin: const EdgeInsets.all(10),
-      icon: Icon(Icons.check),
-    );
-  }
-
-  static errorSnackBar({required title, message = "", duration = 3}) {
-    Get.snackbar(
-      title,
-      message,
-      isDismissible: true,
-      shouldIconPulse: true,
-      colorText: Colors.white,
-      backgroundColor: Color(0xffbb2124),
-      duration: duration,
-      snackPosition: SnackPosition.BOTTOM,
-      margin: const EdgeInsets.all(10),
-      icon: Icon(Icons.error),
-    );
-  }
-
-  static warningSnackBar({required title, message = "", duration = 3}) {
-    Get.snackbar(
-      title,
-      message,
-      isDismissible: true,
-      shouldIconPulse: true,
-      colorText: Colors.white,
-      backgroundColor: Color(0xfff0ad4e),
-      duration: duration,
-      snackPosition: SnackPosition.BOTTOM,
-      margin: const EdgeInsets.all(10),
-      icon: Icon(Icons.warning),
-    );
-  }
-
-  static infoSnackBar({required title, message = "", duration = 3}) {
-    Get.snackbar(
-      title,
-      message,
-      isDismissible: true,
-      shouldIconPulse: true,
-      colorText: Colors.white,
-      backgroundColor: Color(0xff5bc0de),
-      duration: duration,
-      snackPosition: SnackPosition.BOTTOM,
-      margin: const EdgeInsets.all(10),
-      icon: Icon(Icons.info),
-    );
-  }
+  ).show(context!);
 }
